c5b4ab704bd2c6ba20f0bccf18b627cc84cd22b9
lesliepajuelo <l.l.pajuelo@gmail.com>
2015-11-15 13:41:40 -0800

    try openshift

---
+++ Table/table.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('myApp.table', ['ngRoute', 'smart-table', ])
+
+.config(['$routeProvider', function($routeProvider, $http) {
+  $routeProvider.when('/table', {
+    templateUrl: 'Table/table.html',
+    controller: 'filterCtrl'
+  });
+}]).controller('filterCtrl', ['$scope', '$filter', function ($scope, filter) {
+  $scope.sortType = "AREA_NAME";
+  $scope.sortRevers = false;
+  $scope.searchLocation="";
+
+ $scope.dynamicTooltipText = 'dynamic';
+ $scope.dynamicTooltip = 'Hello, World!';
+
+
+
+  //http://lorenzofox3.github.io/smart-table-website/
+    // scope.predicates = ['Areaname', 'Rent50_3'];
+    // scope.selectedPredicate = scope.predicates[0];
+}]).controller("PostsCtrl", function($scope, $http) {
+  //Pulling in housing data, incomplete
+  $http.get('assets/joined.json').
+    success(function(data) {
+      $scope.posts = data;
+    }).
+    error(function(data) {
+      // log error
+    });
+
+    //trying to pull in indeed data
+    
+    var location = 93306
+
+
+    
+
+
+});
